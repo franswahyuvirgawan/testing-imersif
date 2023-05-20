a2c0d74bb4614ef4a8bdfdc7ae3b01aac6a151ad
Frans701 <franswahyu7@gmail.com>
2023-05-20 12:35:05 +0800

    voice Recognition

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,75 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import Test from "./videos/test.mp4";
+import Lagu1 from "./videos/test.mp4";
+import Lagu2 from "./videos/ganti.mp4";
+import Lagu3 from "./videos/play.mp4";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [playVideo, setPlayVideo] = useState(false);
   const [showSuccess, setShowSuccess] = useState(false);
+  const [videoSource, setVideoSource] = useState("");
+  const [videoKey, setVideoKey] = useState(0);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setInputValue("");
-    }, 1000);
+    // Memeriksa apakah browser mendukung Speech Recognition API
+    if ("webkitSpeechRecognition" in window) {
+      const recognition = new window.webkitSpeechRecognition();
 
-    if (inputValue.toLowerCase() === "mulai") {
-      setPlayVideo(true);
-      setShowSuccess(true);
-      setInputValue("");
+      recognition.continuous = true; // Mendeteksi suara terus-menerus
+      recognition.interimResults = true; // Menghasilkan hasil interim saat pengguna berbicara
+
+      recognition.onresult = (event) => {
+        const transcript =
+          event.results[event.results.length - 1][0].transcript;
+        setInputValue(transcript.toLowerCase());
+
+        if (transcript.toLowerCase().includes("halo")) {
+          setPlayVideo(true);
+          setShowSuccess(true);
+          setVideoSource(Lagu1);
+        } else if (transcript.toLowerCase().includes("hola")) {
+          setVideoSource(Lagu2);
+          setPlayVideo(true);
+          setShowSuccess(true);
+        } else if (transcript.toLowerCase().includes("hai")) {
+          setVideoSource(Lagu3);
+          setPlayVideo(true);
+          setShowSuccess(true);
+        }
+      };
+
+      setVideoKey((prevKey) => prevKey + 1);
+
+      recognition.start(); // Memulai recognition
+
+      return () => {
+        recognition.stop(); // Menghentikan recognition saat komponen tidak lagi digunakan
+      };
     } else {
-      setShowSuccess(false);
+      console.log("Speech Recognition tidak didukung di browser ini");
     }
-
-    return () => clearTimeout(timeout);
   }, [inputValue]);
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
 
   return (
     <div>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
       {playVideo && (
         <div>
-          <video width="560" height="315" controls autoPlay>
-            <source src={Test} type="video/mp4" />
+          <video
+            key={videoKey}
+            width="560"
+            height="315"
+            controls
+            autoPlay
+            muted
+          >
+            <source src={videoSource} type="video/mp4" />
           </video>
           {showSuccess && <p>Berhasil!</p>}
         </div>
